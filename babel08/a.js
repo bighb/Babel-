481d5331668893a4fa625f1fd42db7c3640522dd
黄奔 <2740916018@qq.com>
2022-05-05 13:50:12 +0800

    引入angularjs跑ie8

--- babel08/a.js
+++ babel08/a.js
@@ -1,6 +1,19 @@
 // >=IE5
+import angular from "angular";
+console.log("angular: ", angular);
+import hello from "./components/hello";
+
+const ngModule = angular.module("app", []);
+
+console.log(ngModule);
+
+hello(ngModule);
+
 var promise = Promise.resolve("ok");
 console.log(promise);
 promise.then(function (res) {
   console.log(res);
 });
+
+var str = "Hello world";
+console.log('str.endsWith("world"): ', str.endsWith("world"));
